df44dca0d8837a833830c49f612d6f79c6b16239
unknown <sineraytd@code.edu.az>
2023-05-25 01:38:02 +0400

    added

---
+++ Elon Musk Money/index.js
@@ -0,0 +1,453 @@
+let cards = [
+    {
+        id: 1,
+        name: 'BigMac',
+        price: 2,
+        imgSrc: './Assets/Images/big-mac.jpg',
+        count: 0
+    },
+    {
+        id: 2,
+        name: 'Flip Flops',
+        price: 3,
+        imgSrc: './Assets/Images/flip-flops.jpg',
+        count: 0
+    },
+    {
+        id: 3,
+        name: 'Coca-Cola Pack',
+        price: 5,
+        imgSrc: './Assets/Images/coca-cola-pack.jpg',
+        count: 0
+    },
+    {
+        id: 4,
+        name: 'Movie Ticket',
+        price: 12,
+        imgSrc: './Assets/Images/movie-ticket.jpg',
+        count: 0
+    },
+    {
+        id: 5,
+        name: 'Book',
+        price: 15,
+        imgSrc: './Assets/Images/book.jpg',
+        count: 0
+    },
+    {
+        id: 6,
+        name: 'Lobster Dinner',
+        price: 45,
+        imgSrc: './Assets/Images/lobster-dinner.jpg',
+        count: 0
+    },
+    {
+        id: 7,
+        name: 'Video Game',
+        price: 60,
+        imgSrc: './Assets/Images/video-game.jpg',
+        count: 0
+    },
+    {
+        id: 8,
+        name: 'Amazon Echo',
+        price: 2,
+        imgSrc: './Assets/Images/amazon-echo.jpg',
+        count: 0
+    },
+    {
+        id: 9,
+        name: 'Year of Netflix',
+        price: 100,
+        imgSrc: './Assets/Images/year-of-netflix.jpg',
+        count: 0
+    },
+    {
+        id: 10,
+        name: 'Air Jordans',
+        price: 125,
+        imgSrc: './Assets/Images/air-jordans.jpg',
+        count: 0
+    },
+    {
+        id: 11,
+        name: 'Airpods',
+        price: 199,
+        imgSrc: './Assets/Images/airpods.jpg',
+        count: 0
+    },
+    {
+        id: 12,
+        name: 'Gaming Console',
+        price: 299,
+        imgSrc: './Assets/Images/gaming-console.jpg',
+        count: 0
+    },
+    {
+        id: 13,
+        name: 'ADrone',
+        price: 350,
+        imgSrc: './Assets/Images/drone.jpg',
+        count: 0
+    },
+    {
+        id: 14,
+        name: 'Smartphone',
+        price: 699,
+        imgSrc: './Assets/Images/smartphone.jpg',
+        count: 0
+    },
+    {
+        id: 15,
+        name: 'Bike',
+        price: 800,
+        imgSrc: './Assets/Images/bike.jpg',
+        count: 0
+    },
+    {
+        id: 16,
+        name: 'Horse',
+        price: 2500,
+        imgSrc: './Assets/Images/horse.jpg',
+        count: 0
+    },
+    {
+        id: 17,
+        name: 'Acre of Farmland',
+        price: 3000,
+        imgSrc: './Assets/Images/acre-of-farmland.jpg',
+        count: 0
+    },
+    {
+        id: 18,
+        name: 'Designer Handbag',
+        price: 5500,
+        imgSrc: './Assets/Images/designer-handbag.jpg',
+        count: 0
+    },
+    {
+        id: 19,
+        name: 'Hot Tub',
+        price: 6000,
+        imgSrc: './Assets/Images/hot-tub.jpg',
+        count: 0
+    },
+    {
+        id: 20,
+        name: 'Diamond Ring',
+        price: 10000,
+        imgSrc: './Assets/Images/diamond-ring.jpg',
+        count: 0
+    },
+    {
+        id: 21,
+        name: 'Jet Ski',
+        price: 12000,
+        imgSrc: './Assets/Images/jet-ski.jpg',
+        count: 0
+    },
+    {
+        id: 22,
+        name: 'Rolex',
+        price: 15000,
+        imgSrc: './Assets/Images/rolex.jpg',
+        count: 0
+    },
+    {
+        id: 23,
+        name: 'Ford F-150',
+        price: 30000,
+        imgSrc: './Assets/Images/ford-f-150.jpg',
+        count: 0
+    },
+    {
+        id: 24,
+        name: 'Tesla',
+        price: 75000,
+        imgSrc: './Assets/Images/tesla.jpg',
+        count: 0
+    },
+    {
+        id: 25,
+        name: 'Monster Truck',
+        price: 150000,
+        imgSrc: './Assets/Images/monster-truck.jpg',
+        count: 0
+    },
+    {
+        id: 26,
+        name: 'Ferrari',
+        price: 250000,
+        imgSrc: './Assets/Images/ferrari.jpg',
+        count: 0
+    },
+    {
+        id: 27,
+        name: 'McDonalds Franchise',
+        price: 1500000,
+        imgSrc: './Assets/Images/mcdonalds-franchise.jpg',
+        count: 0
+    },
+    {
+        id: 28,
+        name: 'Superbowl Ad',
+        price: 5250000,
+        imgSrc: './Assets/Images/superbowl-ad.jpg',
+        count: 0
+    },
+    {
+        id: 29,
+        name: 'Yacht',
+        price: 7500000,
+        imgSrc: './Assets/Images/yacht.jpg',
+        count: 0
+    },
+    {
+        id: 30,
+        name: 'M1 Abrams',
+        price: 8000000,
+        imgSrc: './Assets/Images/m1-abrams.jpg',
+        count: 0
+    },
+    {
+        id: 31,
+        name: 'Formula 1 Car',
+        price: 15000000,
+        imgSrc: './Assets/Images/formula-1-car.jpg',
+        count: 0
+    },
+    {
+        id: 32,
+        name: 'Apache Helicopter',
+        price: 31000000,
+        imgSrc: './Assets/Images/apache-helicopter.jpg',
+        count: 0
+    },
+    {
+        id: 33,
+        name: 'Mansion',
+        price: 45000000,
+        imgSrc: './Assets/Images/mansion.jpg',
+        count: 0
+    },
+    {
+        id: 34,
+        name: 'Make a Movie',
+        price: 100000000,
+        imgSrc: './Assets/Images/make-a-movie.jpg',
+        count: 0
+    },
+    {
+        id: 35,
+        name: 'Boeing 747',
+        price: 148000000,
+        imgSrc: './Assets/Images/boeing-747.jpg',
+        count: 0
+    },
+    {
+        id: 36,
+        name: 'Mona Lisa',
+        price: 780000000,
+        imgSrc: './Assets/Images/mona-lisa.jpg',
+        count: 0
+    },
+    {
+        id: 37,
+        name: 'Skyscraper',
+        price: 850000000,
+        imgSrc: './Assets/Images/skyscraper.jpg',
+        count: 0
+    },
+    {
+        id: 38,
+        name: 'Cruise Ship',
+        price: 930000000,
+        imgSrc: './Assets/Images/cruise-ship.jpg',
+        count: 0
+    },
+    {
+        id: 39,
+        name: 'NBA Team',
+        price: 2120000000,
+        imgSrc: './Assets/Images/nba-team.jpg',
+        count: 0
+    }
+];
+
+
+let totalAmount = 187000000000;
+const totalAmountElement = document.querySelector("#totalAmount span");
+const cardsContainer = document.getElementById("cards");
+const cartContainer = document.getElementById("cart");
+const cartItemsContainer = document.getElementById("cartItems");
+const totalElement = document.getElementById("total");
+
+let shoppingCart = [];
+
+cards.forEach((card) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    const cardImg = document.createElement("img");
+    cardImg.src = card.imgSrc;
+    cardImg.classList.add("cardImage");
+    cardDiv.appendChild(cardImg);
+
+    const cardName = document.createElement("h3");
+    cardName.textContent = card.name;
+    cardName.classList.add("cardName");
+    cardDiv.appendChild(cardName);
+
+    const cardPrice = document.createElement("h4");
+    const cardPriceSpan = document.createElement("span");
+    cardPriceSpan.textContent = card.price;
+    cardPrice.appendChild(document.createTextNode("$"));
+    cardPrice.appendChild(cardPriceSpan);
+    cardPrice.classList.add("cardPrice");
+    cardDiv.appendChild(cardPrice);
+
+    const cardSellBtn = document.createElement("button");
+    cardSellBtn.textContent = "Sell";
+    cardSellBtn.classList.add("sellButton");
+    cardSellBtn.disabled = true;
+
+    const cardInput = document.createElement("input");
+    cardInput.setAttribute("type", "number");
+    cardInput.setAttribute("min", "0");
+    cardInput.value = 0;
+    cardInput.classList.add("input");
+
+    const cardBuyBtn = document.createElement("button");
+    cardBuyBtn.textContent = "Buy";
+    cardBuyBtn.classList.add("buyButton");
+
+    const operationsDiv = document.createElement("div");
+    operationsDiv.classList.add("operations");
+    operationsDiv.append(cardSellBtn, cardInput, cardBuyBtn);
+    cardDiv.appendChild(operationsDiv);
+
+    cardsContainer.appendChild(cardDiv);
+
+    cardBuyBtn.addEventListener("click", () => {
+        if (card.count < 100) {
+            const totalPrice = card.price;
+            if (totalPrice <= totalAmount) {
+                totalAmount -= totalPrice;
+                card.count += 1;
+                totalAmountElement.textContent = totalAmount.toLocaleString();
+                cardInput.value = card.count;
+                if (card.count === 1) {
+                    cardSellBtn.disabled = false;
+                    cardSellBtn.classList.add("active");
+                }
+
+                const purchasedItem = {
+                    name: card.name,
+                    quantity: card.count,
+                    price: card.price
+                };
+                shoppingCart.push(purchasedItem);
+                updateShoppingCart();
+            }
+        }
+        cartContainer.classList.remove("hide");
+    });
+
+    cardSellBtn.addEventListener("click", () => {
+        if (card.count > 0) {
+            const totalPrice = card.price;
+            totalAmount += totalPrice;
+            card.count -= 1;
+            totalAmountElement.textContent = totalAmount.toLocaleString();
+            cardInput.value = card.count;
+            if (card.count === 0) {
+                cardSellBtn.disabled = true;
+                cardSellBtn.classList.remove("active");
+            }
+
+            const itemIndex = shoppingCart.findIndex(item => item.name === card.name);
+            if (itemIndex !== -1) {
+                shoppingCart[itemIndex].quantity -= 1;
+                if (shoppingCart[itemIndex].quantity === 0) {
+                    shoppingCart.splice(itemIndex, 1);
+                }
+                updateShoppingCart();
+            }
+        }
+    });
+
+    cardInput.addEventListener("input", () => {
+        const newCount = parseInt(cardInput.value);
+        if (!isNaN(newCount) && newCount >= 0 && newCount <= 99) {
+            const diff = newCount - card.count;
+            if (diff > 0) {
+                const totalPrice = diff * card.price;
+                if (totalPrice <= totalAmount) {
+                    totalAmount -= totalPrice;
+                    card.count = newCount;
+                    totalAmountElement.textContent = totalAmount.toLocaleString();
+                    cardSellBtn.disabled = false;
+                    cardSellBtn.classList.add("active");
+
+                    const purchasedItem = {
+                        name: card.name,
+                        quantity: card.count,
+                        price: card.price
+                    };
+                    shoppingCart.push(purchasedItem);
+                    updateShoppingCart();
+                }
+            } else if (diff < 0) {
+                const totalPrice = Math.abs(diff) * card.price;
+                totalAmount += totalPrice;
+                card.count = newCount;
+                totalAmountElement.textContent = totalAmount.toLocaleString();
+
+                const itemIndex = shoppingCart.findIndex(item => item.name === card.name);
+                if (itemIndex !== -1) {
+                    shoppingCart[itemIndex].quantity = newCount;
+                    if (newCount === 0) {
+                        shoppingCart.splice(itemIndex, 1);
+                    }
+                    updateShoppingCart();
+                }
+
+                if (card.count === 0) {
+                    cardSellBtn.disabled = true;
+                    cardSellBtn.classList.remove("active");
+                }
+            }
+        }
+    });
+});
+
+function updateShoppingCart() {
+    cartItemsContainer.innerHTML = "";
+    let cartTotal = 0;
+    shoppingCart.forEach(item => {
+        const cartItemDiv = document.createElement("div");
+        cartItemDiv.classList.add("cartItem");
+
+        const itemName = document.createElement("span");
+        itemName.textContent = item.name;
+        itemName.classList.add("itemName");
+        cartItemDiv.appendChild(itemName);
+
+        const itemQuantity = document.createElement("span");
+        itemQuantity.textContent = "x " + item.quantity;
+        itemQuantity.classList.add("itemQuantity");
+        cartItemDiv.appendChild(itemQuantity);
+
+        const itemPrice = document.createElement("span");
+        itemPrice.textContent = "$" + (item.price * item.quantity);
+        itemPrice.classList.add("itemPrice");
+        cartItemDiv.appendChild(itemPrice);
+
+        cartItemsContainer.appendChild(cartItemDiv);
+
+        cartTotal += item.price * item.quantity;
+    });
+
+    totalElement.textContent = "Total: $" + cartTotal;
+}
+
